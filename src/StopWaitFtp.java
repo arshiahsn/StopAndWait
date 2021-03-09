f309c7e052c420bff82b84366f98c73f257071cf
Arshia <arshiahsn@gmail.com>
2021-03-08 17:33:35 -0700

    implemented all functionalities

--- src/StopWaitFtp.java
+++ src/StopWaitFtp.java
@@ -1,21 +1,91 @@
 
 
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.*;
 
 public class StopWaitFtp {
+
+	public class ResendTimer extends TimerTask {
+		private int timer;
+		private DatagramPacket pkt;
+		private DatagramSocket udpSocket;
+
+
+
+
+		public ResendTimer(int timer_, DatagramPacket pkt_, DatagramSocket udpSocket_){
+			timer = timer_;
+			pkt = pkt_;
+			udpSocket = udpSocket_;
+		}
+
+		@Override
+		public void run() {
+
+			while(true){
+				try {
+					udpSocket.send(pkt);
+					Thread.sleep(timer);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+	}
+
+	public long getFileLen() {
+		return fileLen;
+	}
+
+	public void setFileLen(long fileLen) {
+		this.fileLen = fileLen;
+	}
+
+	private long fileLen;
 	
 	private static final Logger logger = Logger.getLogger("StopWaitFtp"); // global logger	
 
+	public int getServerUdpPort() {
+		return serverUdpPort;
+	}
+
+	public int getInitSeqNo() {
+		return initSeqNo;
+	}
+
+	public void setServerUdpPort(int serverUdpPort) {
+		this.serverUdpPort = serverUdpPort;
+	}
+
+	public void setInitSeqNo(int initSeqNo) {
+		this.initSeqNo = initSeqNo;
+	}
+
+	private int serverUdpPort;
+	private int initSeqNo;
+	public final static int MAX_PAYLOAD_SIZE = 1400; // bytes
+
+	public int getTimeout() {
+		return timeout;
+	}
+
+	private int timeout;
 	/**
 	 * Constructor to initialize the program
 	 *
 	 * @param timeout The time-out interval for the retransmission timer
 	 */
 	public StopWaitFtp(int timeout) {
-
+		this.timeout = timeout;
 
 
 	}
@@ -30,8 +100,46 @@ public class StopWaitFtp {
 	 * @throws FtpException If anything goes wrong while sending the file
 	 */
 	public void send(String serverName, int serverPort, String fileName) throws FtpException {
+		try{
+			int bufferSize = 10000;
+			DatagramSocket udpSocket = new DatagramSocket();
+			handshake(serverName, serverPort, fileName, udpSocket.getLocalPort());
+
+			// create the payload
+			byte[] payload = new byte[MAX_PAYLOAD_SIZE];
+			byte[] buffer = new byte[MAX_PAYLOAD_SIZE];
+			// create a segment with all-zero payload and seqNum 1
 
 
+
+			FileInputStream inFile = new FileInputStream(fileName);
+			int readBytes = 0;
+			int seqNo = getInitSeqNo();
+			long totalReadBytes = 0;
+			Timer timer = new Timer();
+
+			//read from file(inName) then send to server until entire file is sent
+			while ((readBytes = inFile.read(payload)) != -1) {
+				totalReadBytes += readBytes;
+				FtpSegment seg = new FtpSegment(seqNo, payload);
+				FtpSegment ackSeg = new FtpSegment(seqNo+1, buffer);
+				// create a DatagramPacket that can be used to send segment
+				DatagramPacket pkt = FtpSegment.makePacket(seg, InetAddress.getByName(serverName), getServerUdpPort());
+				DatagramPacket ack = FtpSegment.makePacket(ackSeg, InetAddress.getByName(serverName), getServerUdpPort());
+				ResendTimer resendTimer = new ResendTimer(getTimeout(),pkt,udpSocket);
+				timer.scheduleAtFixedRate(resendTimer,0,getTimeout());
+				udpSocket.receive(ack);
+				resendTimer.cancel();
+
+			}
+			timer.cancel();
+			timer.purge();
+
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 
 	}
 
@@ -49,23 +157,17 @@ public class StopWaitFtp {
 			DataInputStream tcpInput = new DataInputStream(tcpSocket.getInputStream());
 			//Send the UTF encoded file name
 			byte[] bytes = fileName.getBytes(StandardCharsets.UTF_8);
-			String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
-			tcpOutput.writeBytes(utf8EncodedString);
+			tcpOutput.writeBytes(new String(bytes, StandardCharsets.UTF_8));
 			//Send the file length
 			File file = new File(fileName);
-			long fileLength = file.length();
-			tcpOutput.writeLong(fileLength);
+			setFileLen(file.length());
+			tcpOutput.writeLong(getFileLen());
 			//Send the local UDP port number
 			tcpOutput.writeInt(udpPort);
 			//Receive the server Port
-			int serverUdpPort = tcpInput.readInt();
+			setServerUdpPort(tcpInput.readInt());
 			//Receive the seq number
-			int initSeqNo = tcpInput.readInt();
-
-
-
-
-
+			setInitSeqNo(tcpInput.readInt());
 		}
 		catch(Exception e){
 			e.printStackTrace();
